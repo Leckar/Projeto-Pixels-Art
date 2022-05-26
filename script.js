d7e3ab5140800754cff967c93c39e650bd075f88
Leckar <gusleckar@gmail.com>
2022-05-25 21:37:12 -0300

    restarting from scratch

--- script.js
+++ script.js
@@ -1,15 +1,45 @@
-const colorPalettes = document.getElementsByClassName("color");
-function randomColorGenerator() {
-	const numberArray = [];
-	for (let i = 1; i < 4; i += 1) {
-		const randomNumber = Math.round(Math.random() * 255);
-		numberArray.push(randomNumber);
-	}
-	return `rgb(${numberArray[0]}, ${numberArray[1]}, ${numberArray[2]})`;
+//Gerador de cores aleatórias para as paletas;
+const colorPalettes = document.querySelectorAll('.color');
+function randomRGBNumber() {
+	return Math.round(Math.random() * 230);
 }
-window.onload = function() {
-	for (let i = 1; i < 4; i += 1) {
-		const randomRgbValues = randomColorGenerator();
-		colorPalettes[i].style.backgroundColor = randomRgbValues;
+function randomRGBColor() {
+	return `rgb(${randomRGBNumber()}, ${randomRGBNumber()}, ${randomRGBNumber()})`;
+}
+function paletteColorSetter() {
+	const paletteSize = colorPalettes.length;
+	for (let i = 0; i < paletteSize; i += 1) {
+		if (i < 1) {
+			colorPalettes[i].style.backgroundColor = 'black';
+		} else {
+			colorPalettes[i].style.backgroundColor = randomRGBColor();
+		}
 	}
+}
+//Gerador do board;
+const pixelBoard = document.getElementById('pixel-board');
+function boardCell() {
+	const newDiv = document.createElement('div');
+	newDiv.classList.add('pixel');
+	return newDiv;
+}
+function boardRows(sideSize) {
+	const newRow = document.createElement('div');
+	newRow.classList.add('row');
+	pixelBoard.appendChild(newRow);
+	for (let i = 0; i < sideSize; i += 1) {
+		const newLine = boardCell();
+		newRow.appendChild(newLine);
+	}
+}
+function boardBuilder(sideSize) {
+	for (let i = 0; i < sideSize; i += 1) {
+		boardRows(sideSize);
+	}
+}
+
+//Função que carrega todos os elementos quando a página carrega;
+window.onload = () => {
+	paletteColorSetter();
+	boardBuilder(5);
 }
